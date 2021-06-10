d336409f7012997531c6356455220908be65e4ec
chae-young <cycy8527@gmail.com>
2021-06-10 22:33:25 +0900

    add: pagnation

--- front/components/Pagination.js
+++ front/components/Pagination.js
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
+import ssss, { useState } from "react"
 
-function usePagination(data, itemsPerPage) {
-  const [currentPage, setCurrentPage] = useState(1);
-  const maxPage = Math.ceil(data.length / itemsPerPage);
+const usePagination = (data, itemsPerPage) => {
+  const [currentPage, setCurrentPage] = useState(1)
+  const maxPage = Math.ceil(data.length / itemsPerPage)
 
   function currentData() {
-    const begin = (currentPage - 1) * itemsPerPage;
-    const end = begin + itemsPerPage;
-    return data.slice(begin, end);
+    const begin = (currentPage - 1) * itemsPerPage
+    const end = begin + itemsPerPage
+    return data.slice(begin, end)
   }
 
   function next() {
-    setCurrentPage(currentPage => Math.min(currentPage + 1, maxPage));
+    setCurrentPage((currentPage) => Math.min(currentPage + 1, maxPage))
   }
 
   function prev() {
-    setCurrentPage(currentPage => Math.max(currentPage - 1, 1));
+    setCurrentPage((currentPage) => Math.max(currentPage - 1, 1))
   }
 
   function jump(page) {
-    const pageNumber = Math.max(1, page);
-    setCurrentPage(currentPage => Math.min(pageNumber, maxPage));
+    const pageNumber = Math.max(1, page)
+    setCurrentPage((currentPage) => Math.min(pageNumber, maxPage))
   }
 
-  return { next, prev, jump, currentData, currentPage, maxPage };
+  return { next, prev, jump, currentData, currentPage, maxPage }
 }
 
-export default usePagination;
+export default usePagination
--- front/components/PostList.js
+++ front/components/PostList.js
@@ -1,32 +1,56 @@
-import React from "react"
+import React, { useState } from "react"
 import { useSelector } from "react-redux"
 import LinesEllipsis from "react-lines-ellipsis"
+import { Pagination } from "@material-ui/lab"
+import usePagination from "./Pagination"
 import { PostListLi } from "../styles/style"
 
 const PostList = () => {
-  const { postList } = useSelector(state => state.post)
+  const { postList } = useSelector((state) => state.post)
+
+  const [page, setPage] = useState(1)
+  const PER_PAGE = 24 // 페이지내 개수
+
+  const count = Math.ceil(postList.length / PER_PAGE)
+  const _DATA = usePagination(postList, PER_PAGE)
+
+  const handleChange = (e, p) => {
+    setPage(p)
+    _DATA.jump(p)
+  }
 
   return (
-    <ul>
-      {postList.map(v => (
-        <PostListLi key={v.id}>
-          {v.Images[0] && (
-            <div className="post-list__img">
-              <img src={v.Images[0].src} alt="" />
+    <>
+      <ul>
+        {postList.map((v) => (
+          <PostListLi key={v.id}>
+            {v.Images[0] && (
+              <div className="post-list__img">
+                <img src={v.Images[0].src} alt="" />
+              </div>
+            )}
+            <div className="post-list__con">
+              <LinesEllipsis
+                text={v.post.title}
+                maxLine="1"
+                ellipsis="..."
+                trimRight
+              />
+              <span>댓글{v.Comments.length}</span>
             </div>
-          )}
-          <div className="post-list__con">
-            <LinesEllipsis
-              text={v.post.title}
-              maxLine="1"
-              ellipsis="..."
-              trimRight
-            />
-            <span>댓글{v.Comments.length}</span>
-          </div>
-        </PostListLi>
-      ))}
-    </ul>
+          </PostListLi>
+        ))}
+      </ul>
+
+      <Pagination
+        count={count}
+        size="large"
+        page={page}
+        variant="outlined"
+        shape="rounded"
+        onChange={handleChange}
+      />
+    </>
   )
 }
 
