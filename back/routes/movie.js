d65ff66e33c047f9a973df60ba40f0cdbb09858e
chae-young <cycy8527@gmail.com>
2021-06-21 21:00:02 +0900

    add:cheerio movie list

--- back/routes/movie.js
+++ back/routes/movie.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const dotenv = require('dotenv');
+const cheerio = require('cheerio');
 
 dotenv.config();
 
@@ -15,8 +16,9 @@ router.get('/', async (req, res, next) => {
             'X-Naver-Client-Secret': client_secret
         },
         params:{
-            query:req.query.query
-        }
+            query:req.query.query,
+            display:20,
+        },
     };
     try{
         const result = await axios('https://openapi.naver.com/v1/search/movie.json',options)
@@ -25,8 +27,31 @@ router.get('/', async (req, res, next) => {
         console.error(error);
         next(error);
     }
+});
 
+router.get('/main', async (req, res, next) => {
+    try{
+        const result = await axios('https://movie.naver.com/movie/running/current.nhn?view=list&tab=normal&order=reserve')
+        let crawledMovie = [];
+        const $ = cheerio.load(result.data);
+        const $movieList = $('.lst_detail_t1').children('li');
 
- });
+        $movieList.each(function (i) {
+            if(i < 20){                
+                crawledMovie[i] = {
+                  title: $(this).find('.tit a').text(),
+                  img: $(this).find('.thumb img').attr('src'),
+                  star: $(this).find('.num').text()
+                };
+            }
+        });
+
+        return res.status(201).json(crawledMovie);
+    }catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
 
  module.exports = router;
