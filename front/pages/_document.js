ba4697c71ab4e0a7ab8a3fb23b9594247dd87aad
chae-young <cycy8527@gmail.com>
2021-08-31 21:09:36 +0900

    Add font

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Document, { Html, Head, Main, NextScript } from "next/document"
-import { ServerStyleSheets } from "@material-ui/core/styles"
 import { ServerStyleSheet } from "styled-components"
+import { ServerStyleSheets } from "@material-ui/core/styles"
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -16,8 +16,10 @@ export default class MyDocument extends Document {
         })
 
       const initialProps = await Document.getInitialProps(ctx)
+      const styleTags = sheet.getStyleElement()
       return {
         ...initialProps,
+        styleTags,
         styles: (
           <>
             {initialProps.styles}
@@ -33,7 +35,13 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap"
+            rel="stylesheet"
+          />
+          {this.props.styleTags}
+        </Head>
         <body>
           <Main />
           <script src="https://polyfill.io/v3/polyfill.min.js?features=default%2Ces2016%2Ces2015%2Ces2017%2Ces2018%2Ces2019" />
@@ -57,7 +65,6 @@ MyDocument.getInitialProps = async (ctx) => {
 
   return {
     ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
     styles: [
       ...React.Children.toArray(initialProps.styles),
       sheets.getStyleElement(),
--- front/styles/GlobalStyle.js
+++ front/styles/GlobalStyle.js
@@ -1,23 +1,27 @@
 import { createGlobalStyle } from "styled-components"
 import reset from "styled-reset"
+import { headerHeight } from "./style"
 
 const GlobalStyle = createGlobalStyle`
-    ${reset};
+${reset};
     html,body{
         padding: 0;
         margin: 0;
+        font-family:-apple-system, BlinkMacSystemFont, "Noto Sans KR", Roboto, "Segoe UI", "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";   
         font-size:10px;
     };
     body{
+        padding-top:${headerHeight};
+        line-height: 1.5;
         background:#f5f0e4;        
     }
     button{
         display: flex;
         cursor: pointer;
         outline: none;
-        border-radius: 3px;
         background:none;
         border:0;
+        padding:0;
     };
     input{
         display: flex;
@@ -27,6 +31,9 @@ const GlobalStyle = createGlobalStyle`
     a{
         text-decoration:none;
         color:rgb(0,0,0);
+    }
+    li{
+        list-style:none;
     }
 `
 export default GlobalStyle
