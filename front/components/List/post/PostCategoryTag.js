0a8e9147a8194e0fcd6f469f4b885b41dfc331da
chae-young <cycy8527@gmail.com>
2021-09-02 22:24:24 +0900

    Add category component

---
+++ front/components/List/post/PostCategoryTag.js
@@ -0,0 +1,25 @@
+import React, { useState, useCallback, useEffect } from "react"
+import PropTypes from "prop-types"
+
+import styled from "styled-components"
+
+const CategoryTag = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  padding: 0.3rem 0.5rem;
+  font-size: 1.6rem;
+  color: rgb(0, 0, 0);
+  background: ${({ name }) =>
+    (name === "MOVIE" && "rgb(102 178 107)") ||
+    (name === "DRAMA" && "rgb(144 210 211)") ||
+    "rgb(252 145 72)"};
+`
+
+const PostCategoryTag = ({ category }) => {
+  return <CategoryTag name={category}>{category}</CategoryTag>
+}
+PostCategoryTag.propTypes = {
+  category: PropTypes.string.isRequired,
+}
+export default PostCategoryTag
